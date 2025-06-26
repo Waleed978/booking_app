cc4674f8d72daf238b4dd15f778e805d0a7d0b33
Waleed978 <waleed.tariq@invicttus.com>
2025-06-26 15:30:31 +0500

    lot of things changed in man

--- main.go
+++ main.go
@@ -7,5 +7,18 @@ func main() {
 	age := 25
 
 	fmt.Printf("My name is %s and I am %T years old.\n", name, age)
-	fmt.Printf("My name is %s and I am %T years old.\n", name, age)
+	msg := fmt.Sprintf("Hi %s, your open rate is %.1f percent", name, 10.12)
+	fmt.Println(msg)
+	if age > 18 {
+		fmt.Println("You are an adult")
+	} else {
+		fmt.Println("You are a minor")
+	}
+	sum, sub := addAndSub(10, 20)
+	fmt.Printf("The sum of %d and %d is %d\n", 10, 20, sum)
+	fmt.Printf("The difference of %d and %d is %d\n", 10, 20, sub)
+
+}
+func addAndSub(a int, b int) (int, int) {
+	return a + b, a - b
 }

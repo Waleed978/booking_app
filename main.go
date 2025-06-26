f40049dfdd04fe3ff6245bcdb587f5dabd2aecd8
Waleed978 <waleed.tariq@invicttus.com>
2025-06-26 14:41:03 +0500

    3 prints removed

--- main.go
+++ main.go
@@ -7,8 +7,4 @@ func main() {
 	age := 25
 
 	fmt.Printf("My name is %s and I am %T years old.\n", name, age)
-	fmt.Printf("My name is %s and I am %T years old.\n", name, age)
-	fmt.Printf("My name is %s and I am %T years old.\n", name, age)
-	fmt.Printf("My name is %s and I am %T years old.\n", name, age)
-
 }
